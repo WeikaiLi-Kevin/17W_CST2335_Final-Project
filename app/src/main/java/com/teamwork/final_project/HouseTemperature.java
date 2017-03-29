ae176e33e179a2ef89658326ac2dd2335c3655fe
yuan0451 <yuan0040@algonquinlive.com>
2017-03-29 11:39:59 -0400

    House Setting Milestone 1

--- app/src/main/java/com/teamwork/final_project/HouseTemperature.java
+++ app/src/main/java/com/teamwork/final_project/HouseTemperature.java
@@ -3,33 +3,27 @@ package com.teamwork.final_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    protected static final String ACTIVITY_NAME = "MainActivity";
+public class HouseTemperature extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.house_activity_house_temperature);
 
-        Button houseBtn = (Button) findViewById(R.id.house);
-        houseBtn.setOnClickListener(new View.OnClickListener() {
+        Button enterBtn = (Button) findViewById(R.id.temperature);
+        enterBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HouseListView.class);
+                Intent intent = new Intent(HouseTemperature.this, HouseListView.class);
                 startActivityForResult(intent, 5);
             }
         });
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 5) {
-            Log.i(ACTIVITY_NAME, "Returned to MainActivity.onActivityResult");
         }
-
     }
-
 }
--- app/src/main/java/com/teamwork/final_project/HouseWeather.java
+++ app/src/main/java/com/teamwork/final_project/HouseWeather.java
@@ -3,33 +3,28 @@ package com.teamwork.final_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    protected static final String ACTIVITY_NAME = "MainActivity";
+public class HouseWeather extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.house_activity_house_weather);
 
-        Button houseBtn = (Button) findViewById(R.id.house);
-        houseBtn.setOnClickListener(new View.OnClickListener() {
+        Button enterBtn = (Button) findViewById(R.id.weather);
+        enterBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HouseListView.class);
+                Intent intent = new Intent(HouseWeather.this, HouseListView.class);
                 startActivityForResult(intent, 5);
             }
         });
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 5) {
-            Log.i(ACTIVITY_NAME, "Returned to MainActivity.onActivityResult");
         }
-
     }
 
 }
